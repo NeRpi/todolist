2cc5cf90f61b4fe4601f801c2860f831d1ee8681
NeRpi <dmitrykruk12@gmail.com>
2023-11-24 16:53:25 +0300

    init: connect mongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
 import cors from "cors";
-import "dotenv";
+import "dotenv/config";
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hi!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running ad localhost:${port}`);
-});
+const start = async () => {
+  mongoose.connect(process.env.DB_CONNECTION_STRING!);
+  app.listen(port, () => {
+    console.log(`Server is running ad localhost:${port}`);
+  });
+};
+
+start();
